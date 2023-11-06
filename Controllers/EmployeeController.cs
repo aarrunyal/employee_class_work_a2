34cf58952f18c118ace8b73ba16b4e2e16a2c015
aarrunyal <aarrunyal@gmail.com>
2023-11-06 08:58:30 -0500

    project created

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,71 @@
+﻿using AssignmentEmployee.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssignmentEmployee.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(Repository.AllEmployees);
+        }
+
+        //Http GET verison
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //HTTP POST version
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.Create(employee);
+                return View("Thanks", employee);
+            }
+            else {
+                return View();
+            }
+        }
+
+        public IActionResult Update(string empname)
+        {
+            Employee employee = Repository.AllEmployees.Where(e => e.Name ==
+           empname).FirstOrDefault();
+            return View(employee);
+        }
+        [HttpPost]
+        public IActionResult Update(Employee employee, string empname)
+        {
+            if (ModelState.IsValid)
+            {
+
+                Repository.AllEmployees.Where(e => e.Name == empname).FirstOrDefault().Age =
+           employee.Age;
+                Repository.AllEmployees.Where(e => e.Name == empname).FirstOrDefault().Salary =
+               employee.Salary;
+                Repository.AllEmployees.Where(e => e.Name == empname).FirstOrDefault().Department
+               = employee.Department;
+                Repository.AllEmployees.Where(e => e.Name == empname).FirstOrDefault().Sex =
+               employee.Sex;
+                Repository.AllEmployees.Where(e => e.Name == empname).FirstOrDefault().Name =
+               employee.Name;
+                return RedirectToAction("Index");
+            }
+            else { 
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(string empname)
+        {
+            Employee employee = Repository.AllEmployees.Where(e => e.Name ==empname).FirstOrDefault();
+            Repository.Delete(employee);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AssignmentEmployee.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage ="Please enter your name")]
+        public string Name { get; set; }
+
+        [Range(16,60,ErrorMessage = "Ages 16-60 only")]
+        public string Age { get; set; }
+        
+        [RegularExpression(@"\d+(\.\d{1,2})?", ErrorMessage = "Invalid, enter like # or #.## ")]
+        public string Salary { get; set; }
+        
+        public string Department { get; set; }
+        
+        [RegularExpression(@"^[MF]+$", ErrorMessage = "Select any one option")]
+        public string Sex{ get; set; }
+    }
+}
